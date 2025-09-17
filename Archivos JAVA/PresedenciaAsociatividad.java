824183ea76a524b55398d214fae5f66fe02bcbe5
Hernan <jhernanmt@gmail.com>
2025-09-17 09:39:55 -0300

    Primeros Trabajos

---
+++ Archivos JAVA/PresedenciaAsociatividad.java
@@ -0,0 +1,11 @@
+public class PresedenciaAsociatividad{
+	public static void main(String[] args) {
+		int value1 = 10/2+10*2-1;
+		System.out.println(value1);
+
+		System.out.println("Resultado es : " + 5 + 5);
+
+		boolean valor = true;
+		System.out.println(!valor);
+	}
+}
